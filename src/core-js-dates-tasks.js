1ffdfac6eb264154bbf7e66c954ad9d49f7411fc
pashaSok <syffiks1998@gmail.com>
2024-03-25 20:36:09 +0300

    fix: formatDate function solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -163,25 +163,22 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
+const options = {
+  month: 'numeric',
+  day: 'numeric',
+  year: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: true,
+  timeZone: 'UTC',
+};
+
+const dateTimeFormatter = new Intl.DateTimeFormat('en-US', options);
+
 function formatDate(date) {
   const dateObject = new Date(date);
-
-  const options = {
-    month: 'numeric',
-    day: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: true,
-    timeZone: 'UTC',
-  };
-
-  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(
-    dateObject
-  );
-
-  return formattedDate;
+  return dateTimeFormatter.format(dateObject);
 }
 
 /**
